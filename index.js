85d23133bb8b106065ddb4d40226e3f5628330af
preetu391 <preetu391391@gmail.com>
2023-04-21 18:10:33 +0530

    ..

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 const express = require("express");
 const app = express()
 
-app.post()
+app.listen(5000, ()=>{
+    console.log("listening port 5000")
+})
+
+app.get("/",(req,res)=>{
+    return res.json("hello world")
+})
